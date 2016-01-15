b5f562af31a0c3a6f0bec3ff95dccc160d66861a
aoudin <ext-aoudin@parkeon.com>
2016-01-15 15:03:59 +0100

    manage proxy configuration for wget to download ftp debian package over proxy

--- contrib/proxy_debian.sh
+++ contrib/proxy_debian.sh
@@ -2,6 +2,15 @@
 #
 # configure a proxy when http_proxy is defined on the system
 #
+
+parse_login() {
+  echo $1 | sed -nE "s/^(http(s)?:\/\/)?((.+):(.+)@)?(.+):([0-9]+)\/?$/\4/p"
+}
+
+parse_password() {
+  echo $1 | sed -nE "s/^(http(s)?:\/\/)?((.+):(.+)@)?(.+):([0-9]+)\/?$/\5/p"
+}
+
 if [ ! -z "$http_proxy" ] &&  [ ! "$http_proxy" = '"nil"' ]; then
     echo "setting up proxy"
 
@@ -19,6 +28,9 @@ if [ ! -z "$http_proxy" ] &&  [ ! "$http_proxy" = '"nil"' ]; then
     export ftp_proxy=$(echo $http_proxy | sed 's/^http/ftp/g')
     export socks_proxy=$(echo $http_proxy | sed 's/^http/socks/g')
     export no_proxy=localhost,127.0.0.1,10.0.0.0/16
+    
+    login=$(parse_login $http_proxy)
+    password=$(parse_password $http_proxy)
 
     if [ "$(id -u)" = "0" ] ; then # should only be done by root
       echo "configure apt proxy"
@@ -32,6 +44,17 @@ Acquire::https::proxy "${http_proxy}";
 Acquire::ftp::proxy "${ftp_proxy}";
 Acquire::socks::proxy "${socks_proxy}";
 APT_CONF
+
+      # configure wget
+      cat > ~/.wgetrc <<EOL
+    http_proxy = http://proxy.parkeon.com:9090/
+    https_proxy =
+    proxy_user = $login
+    proxy_password = $password
+    use_proxy = on
+    wait = 15
+EOL
+
     fi
 else
     echo "skiping proxy settings"
